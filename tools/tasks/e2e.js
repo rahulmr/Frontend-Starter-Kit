cce6acc635ce42e396df7d8fce39356249984ac1
Rahul Raut <rahul.raut1@globallogic.com>
2017-03-20 19:02:13 +0530

    Added webdriver as dependency

--- tools/tasks/e2e.js
+++ tools/tasks/e2e.js
@@ -7,7 +7,7 @@ import { Protractor } from '../utils';
 
 gulp.task('webdriver', webdriver_update);
 
-gulp.task('e2e', (done) => {
+gulp.task('e2e', ['webdriver'], (done) => {
   new Protractor()
     .server(TEST_PORT, DIST_ROOT)
     .then((server) => {
